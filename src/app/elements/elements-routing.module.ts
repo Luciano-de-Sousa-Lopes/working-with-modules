45b0fa3b6857d4de7b4a6926ebe6451f30ec84c2
Luciano de Sousa Lopes <lucianodesousalopes.github@gmail.com>
2022-03-07 22:12:20 -0300

    feat: project created and implemented the module system

--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class ElementsRoutingModule { }
