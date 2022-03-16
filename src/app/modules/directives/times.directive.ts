8fe441185182de91bd1f8cb733ed68c5d9cca79f
Luciano de Sousa Lopes <lucianodesousalopes.github@gmail.com>
2022-03-16 03:23:09 -0300

    feat: added a directive

--- src/app/modules/directives/times.directive.ts
+++ src/app/modules/directives/times.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ViewContainerRef, TemplateRef } from '@angular/core';
 
 @Directive({
-  selector: '[appTimes]'
+  selector: '[appTimes]',
 })
 export class TimesDirective {
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) {}
 
-  constructor() { }
-
+  @Input('appTimes') set times(numberOfTimes: number) {
+    this.viewContainerRef.clear();
+    for (let i = 0; i < numberOfTimes; i++) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef, {
+        index: i,
+      });
+    }
+  }
 }
